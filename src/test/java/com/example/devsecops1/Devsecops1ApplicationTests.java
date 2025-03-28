ffad53115d90a4e46899e3920a44f61a460e4164
charroux <benoit.charroux@gmail.com>
2025-03-28 09:08:35 +0100

    CI

--- src/test/java/com/example/devsecops1/Devsecops1ApplicationTests.java
+++ src/test/java/com/example/devsecops1/Devsecops1ApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.devsecops1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Devsecops1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Car car = new Car("Ferrari", 2000);
+		String brand = car.getBrand();
+		assertEquals(brand, "Ferrari");
 	}
 
 }
